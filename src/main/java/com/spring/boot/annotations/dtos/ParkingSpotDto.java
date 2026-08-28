d616cd3118ac07fd9b8515057fe7d2ccd52322d4
igord <igordf2502@gmail.com>
2026-08-28 16:10:35 -0300

    ajuste import dto

--- src/main/java/com/spring/boot/annotations/dtos/ParkingSpotDto.java
+++ src/main/java/com/spring/boot/annotations/dtos/ParkingSpotDto.java
@@ -1,7 +1,8 @@
 package com.spring.boot.annotations.dtos;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class ParkingSpotDto {
 
